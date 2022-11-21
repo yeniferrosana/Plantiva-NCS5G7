908f6c00fe7ffca0e6502fa43dd8b154c0da5d12
Onnichan <dyangeltk17@gmail.com>
2022-11-21 08:47:49 -0500

    :sparkles: Fix login validations

--- src/components/input/SPInput.vue
+++ src/components/input/SPInput.vue
@@ -1,3 +1,7 @@
+<script setup>
+defineProps(["modelValue"]);
+defineEmits(["update:modelValue"]);
+</script>
 <template>
-  <input type="text" />
+  <input type="text" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"/>
 </template>

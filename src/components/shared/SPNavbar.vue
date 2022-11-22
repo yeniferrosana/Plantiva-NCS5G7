87eb940211f2215c65f0e84c1caf5f6315debdf2
Yenifer Ramírez <yeniferramirez11@gmail.com>
2022-11-22 10:21:39 -0400

    Responsive Navbar

--- src/components/shared/SPNavbar.vue
+++ src/components/shared/SPNavbar.vue
@@ -1,52 +1,55 @@
 <script setup></script>
 
 <template>
-  <nav x-data="{ isOpen: false }" class="relative bg-sp shadow dark:bg-gray-800">
-    <div class="container px-6 py-3 mx-auto md:flex">
-        <div class="flex items-center justify-between">
-            <div>
-                <a class="text-2xl font-bold text-white transition-colors duration-300 transform lg:text-3xl hover:text-sp-100" href="/">LOGO</a>
-            </div>
+  <header class="bg-sp shadow">
+    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <!--Logo-->
+      <div class="my-1">
+        <a class="text-2xl font-bold text-white transition-colors duration-300 transform lg:text-3xl hover:text-sp-100" href="/">LOGO</a>
+      </div>
+      <!--Buscador y Explorar-->
+      <div 
+        v-if="$route.name === 'landing'"
+        class="items-center justify-center md:ml-auto md:mr-auto"
+      >
+        <router-link
+          to="/home"
+        >
+          <button class="inline-flex items-center text-white text-2xl font-bold border-0 py-2 px-3 focus:outline-none rounded-lg md:mt-0 mr-2 hover:bg-white hover:text-sp-50 transition-colors duration-300">
+            EXPLORA
+          </button>
+        </router-link>
+      </div>
+      <div 
+        v-else
+        class="relative items-center justify-center md:ml-auto md:mr-auto"
+      >
+        <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+          <svg class="w-5 h-5 text-sp" viewBox="0 0 24 24" fill="none">
+            <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+          </svg>
+        </span>
+        <input type="text" class="w-full py-2 pl-10 pr-4 text-sp bg-sp-200 border rounded-lg focus:border-sp-100 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-sp-100" placeholder="Buscar Plantas Nativas">
+      </div>
+      
 
-            <!-- Mobile menu button -->
-            <div class="flex lg:hidden">
-                <button x-cloak @click="isOpen = !isOpen" type="button" class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
-                    <svg x-show="!isOpen" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M4 8h16M4 16h16" />
-                    </svg>
-            
-                    <svg x-show="isOpen" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
-        </div>
-
-        <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
-        <div x-cloak :class="[isOpen ? 'translate-x-0 opacity-100 ' : 'opacity-0 -translate-x-full']" class="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-sp dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:opacity-100 md:translate-x-0 md:flex md:items-center md:justify-between">
-            <div class="flex flex-col px-2 -mx-4 md:flex-row md:mx-10 md:py-0">
-                <a href="#" class="px-2.5 py-2 text-white transition-colors duration-300 transform rounded-lg hover:text-gray-700 dark:text-gray-200 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 md:mx-2">Home</a>
-                <a href="#" class="px-2.5 py-2 text-white transition-colors duration-300 transform rounded-lg hover:text-gray-700 dark:text-gray-200 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 md:mx-2">About</a>
-                <a href="#" class="px-2.5 py-2 text-white transition-colors duration-300 transform rounded-lg hover:text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 md:mx-2">Contact</a>
-            </div>
-
-            <div class="relative mt-4 md:mt-0">
-                <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-                    <svg class="w-5 h-5 text-white" viewBox="0 0 24 24" fill="none">
-                        <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                    </svg>
-                </span>
-
-                <input type="text" class="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300" placeholder="Buscar Plantas Nativas">
-            </div>
-        </div>
+      <!--Botones-->
+      <div class="flex text-sp-50 font-bold text-sm mt-2">
+        <router-link
+          to="/register"
+        >
+          <button class="inline-flex items-center bg-sp-100 border-0 py-2 px-3 focus:outline-none rounded-lg md:mt-0 mr-2">
+            REGISTRARME
+          </button>
+        </router-link>
+        <router-link
+          to="/login"
+        >
+          <button class="inline-flex items-center bg-sp-200 border-0 py-2 px-3 focus:outline-none rounded-lg md:mt-0 ml-2">
+            INGRESAR
+          </button>
+        </router-link>
+      </div>
     </div>
-  </nav>
+  </header>
 </template>
-<script>
-export default {
-  data() {
-    return { isOpen: false }
-  },
-}
-</script>

2854c91ac92e2da1fd5b6151085438258d0292ca
Yenifer Ramírez <yeniferramirez11@gmail.com>
2022-12-09 15:28:02 -0400

    Loader test

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <script>
 // import { RouterLink, RouterView } from "vue-router";
+import { ref, onMounted } from 'vue'
 import NavBarVue from "../src/components/shared/SPNavbar.vue";
 import FooterVue from "../src/components/shared/SPFooter.vue";
+import LoaderFull from "../src/components/shared/LoaderFull.vue";
 export default {
   components: {
     NavBarVue,
     FooterVue,
   },
+  data () {
+    return {
+      fullscreen: true,
+    }
+  },
 };
+
+onMounted(() => {
+  this.fullscreen = true;
+});
 </script>
 <template>
+<LoaderFull
+      v-show="fullscreen"
+    />
   <NavBarVue />
   <RouterView class="bg-amber-50" />
   <FooterVue class="fixed w-full bottom-0" />
---
+++ src/components/shared/LoaderFull.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="loading">
+    <img
+      id="loading-image"
+      src="https://res.cloudinary.com/yeniferramirez11/image/upload/v1644072991/metaltek-web/loading_jrtpiq.gif"
+      alt="Loading"
+    ><img>
+  </div>
+</template>
+
+<style scoped>
+  #loading {
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    /*opacity: 0.1;*/
+    background-color: #f1f2f3;
+    z-index: 99;
+  }
+
+  #loading-image {
+    z-index: 100;
+  }
+</style>
